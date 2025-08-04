454a3bedec1f5cf564a73d6a9daa26306aa26bda
Jennifer Akosua Sunkwa <jasunkwa@st.ug.edu.gh>
2025-08-04 23:25:23 +0000

    task three completed

---
+++ TriangleTypeIdentifier/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter three sides of a triangle:");
+
+        Console.Write("Side 1: ");
+        int side1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Side 2: ");
+        int side2 = int.Parse(Console.ReadLine());
+
+        Console.Write("Side 3: ");
+        int side3 = int.Parse(Console.ReadLine());
+
+        if (side1 == side2 && side2 == side3)
+            Console.WriteLine("The triangle is Equilateral.");
+        else if (side1 == side2 || side1 == side3 || side2 == side3)
+            Console.WriteLine("The triangle is Isosceles.");
+        else
+            Console.WriteLine("The triangle is Scalene.");
+    }
+}
