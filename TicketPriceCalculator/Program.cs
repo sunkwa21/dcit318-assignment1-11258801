ed3cc6c08f75d21deaade5fdfe86efb764d47d22
Jennifer Akosua Sunkwa <jasunkwa@st.ug.edu.gh>
2025-08-04 23:22:52 +0000

    task two completed

---
+++ TicketPriceCalculator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter your age: ");
+        int age = int.Parse(Console.ReadLine());
+
+        int price;
+
+        if (age <= 12 || age >= 65)
+            price = 7;
+        else
+            price = 10;
+
+        Console.WriteLine($"Your ticket price is: GHC{price}");
+    }
+}
